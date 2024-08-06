9af2a5733fd4d13e25bff21a9ce25495e442b73a
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-08-06 14:53:26 -0300

    função print

--- aula1.py
+++ aula1.py
@@ -1,2 +1,14 @@
 # Permite escrever um comentário de 1 linha
-print(123)
+print(123) #frente
+#abaixo
+
+#Docstring: """""" não é um comentário pois o python n ignora
+"""
+escrever oq quiser
+pulando linhas
+"""
+
+'''
+usar para escrever notas
+com mais de uma linha
+'''
---
+++ aula2.py
@@ -0,0 +1,4 @@
+# \r\n -> CRLF
+# \n -> LF
+print(12, 34, sep='-', end='##\n')
+print(23, 43, sep="__ ")

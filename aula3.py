54c434385c0c61e74aff2869b0a4e2f6dfa33be0
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-08-07 09:48:09 -0300

    tipo int e float

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = linguagem de programação
+Tipo de tipagem do python = Dinâmica(já sabe o tipo de dado passado) / Forte
+str -> string -> texto que está dentro de aspas(simples ou duplas)
+"""
+
+print(1234)#python já sabe que é um número inteiro sem necessidade de especificar
+
+# Aspas simples
+print('Matheus Kenji')
+print('Matheus "Kenji"')
+
+# Aspas Duplas
+print("Matheus Kenji Nishimura")
+
+# Escape
+print("Matheus \"Kenji\" ")
+
+#r
+
---
+++ aula4.py
@@ -0,0 +1,14 @@
+#Tipos int e float 
+#int -> número inteiro
+#int representa qualquer número positivo e negativo inteiro
+print(11) #int
+print(-11) #int
+print(0) #int
+
+#float -> Número com ponto flutuante
+print(1.1, 0.0, -1.5)
+
+#A "função" type mostra o tipo que o Python inferiu ao valor
+print(type('Kenji'))
+print(type(1))
+print(type(1.2))

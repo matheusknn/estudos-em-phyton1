80f7b745221c97a52f4bbf1e65d9eb3c5ff7b556
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-08-13 11:12:06 -0300

    introdução variáveis

---
+++ aula7.py
@@ -0,0 +1,16 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# nome_completo = 'Matheus Kenji Nishimura'
+# soma_dois_mais_dois = 2 + 2
+# print(nome_completo)
+# print(soma_dois_mais_dois)
+
+nome = 'Matheus'
+idade = 21
+maior_de_idade = idade >= 18
+print(maior_de_idade)

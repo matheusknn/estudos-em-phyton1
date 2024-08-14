d9a7a3a490252ac1706948e5ecf7ae234f94daa6
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-08-14 14:05:56 -0300

    função format para formatar strings

---
+++ aula13.py
@@ -0,0 +1,5 @@
+a = 'AAAA'
+b = 'BBBB'
+c = 1.1
+formato = 'a={0} B={1} C={nome1:.2f}'.format(a, b, nome3 = c) #cada{} será substituida pelas variáveis do format de acordo com os índices que começam de 0
+print(formato)

d9a784389f8d4f1201f90967ffc30127d0d97f02
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-08-19 13:58:57 -0300

    FORMATAÇÃO DE DADOS COM F STRING

---
+++ aula23.py
@@ -0,0 +1,22 @@
+"""
+Formatação básica de strings
+s - string
+d - int
+f - float
+.<número de dígitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+= - Força o número a aparecer antes dos zeros
+Sinal - + ou -
+Ex.: 0>-100,.1f
+Conversion flags - !r !s !a 
+"""
+
+variavel = 'ABC'
+print(f'{variavel: >10}') # > preenche os espços na esquerda de acordo com o número colocado na direita
+print(f'{variavel:#>10}') 
+print(f'{variavel: <10}')
+print(f'{21123312.123213123123123123213:,.3f}')

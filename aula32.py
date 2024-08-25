e3586e25529bf6fa650b629f6a0c52fb92daa240
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-08-25 13:04:31 -0300

    operadores de atribuição

--- aula32.py
+++ aula32.py
@@ -5,8 +5,16 @@ Executa uma ação enquanto uma condição for verdadeira
 Loop infinito -> Quando um código não tem fim
 """
 
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
 contador = 0
 
-while contador <= 10:
-  print(contador)
-  contador = contador + 1
+# while contador <= 10:
+#   print(contador)
+#   contador = contador + 1
+
+contador += 3
+print(contador)

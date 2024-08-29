75d3d8a44c687f95fdd6b5152b637fb596cc56d0
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-08-29 09:33:38 -0300

    for in

---
+++ aula38.py
@@ -0,0 +1,13 @@
+# senha_salva = '123456'
+# senha_digitada = ''
+# repeticoes = 0
+
+# while senha_digitada != senha_salva:
+#   senha_digitada = input(f'sua senha ({repeticoes}x): ')
+
+#   repeticoes += 1
+
+texto = 'Python'
+
+for letra in texto: #para cada letra em texto faça uma interação
+  print(letra)

14f8c3310421875a364eabeddc23d3abc579471d
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-09-02 09:26:00 -0300

    tuplas

--- aula49.py
+++ aula49.py
@@ -1,5 +1,6 @@
 """
 Introdução ao empacotamento e desempacotamento
+Tipo tupla - lista imutável
 """
 
 nomes = ['Matheus', 'Kenji', 'Nishimura']
@@ -7,4 +8,10 @@ nomes = ['Matheus', 'Kenji', 'Nishimura']
 # print(nome2) #nome2 recebe kenji
 
 _, nome2, *_ = nomes #_ indica que a variável n será utilizada
-print(nome2, _) #* retorna os valores que não foram inclusos em nenhuma variável
+print(nome2, _) #* retorna os valores que não foram inclusos em nenhuma variável]
+
+
+#--------------------------Tupla(lista imutável)---------------------------------------------------
+personagens = 'Jiraya', 'Naraito', 'Kushina'
+nomes_tupla = tuple(nomes)
+# personagens[1] = 'Teta' #gera erro, pois uma tupla é um tipo de dado imutável

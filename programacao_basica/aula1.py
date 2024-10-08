4293d4891f5eea346527238c8f58192f3adeec60
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-10-08 09:37:14 -0300

    comentários e DocStrings como comentários

---
+++ programacao_basica/aula1.py
@@ -0,0 +1,17 @@
+"""
+DocString
+escrevendo
+comentários
+de mais de uma linha
+"""
+
+'''
+DocString
+escrevendo
+comentários
+de mais de uma linha
+'''
+
+# permite escrever um comentário
+print(123) #comentário na frente
+# comentário abaixo

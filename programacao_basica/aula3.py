e69c4c2fc9a389e785dfbff159bb08f8dfda219c
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-10-08 10:13:40 -0300

    função print e string(str)

---
+++ programacao_basica/aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica(python já sabe o tipo da informação passada para ele) / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print('Luiz Otávio')
+print(1, 'Luiz "Otávio"')
+
+# Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'")
+
+# Escape
+print("Luiz \"Otávio\"")
+print("Matheus \"Kenji\"") #\ serve como escapa para o caractere que vier após ele
+
+# r
+print(r"Luiz \"Otávio\"") #faz o caractere de escape ser exibido

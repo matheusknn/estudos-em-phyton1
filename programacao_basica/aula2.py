ef19c7cc5fdbc29ff74db1511db62e7320b0b221
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-10-08 09:59:04 -0300

    função print e seus parâmetros

---
+++ programacao_basica/aula2.py
@@ -0,0 +1,6 @@
+# \r\n -> CRLF - os dois significam quebra de linha
+# \n -> LF
+# sep define como os argumentos colocados da função prints serão separados o padrão é por espaço
+# end define o que vem depois dos argumentos colocados na função print, o pdrão é uma quebra de linha(\n)
+print(12, 34, sep=" - ", end='\n')
+print(56, 78, sep= ' - ')

300415cf82a0b8275c2e7e7b39fc9ffb2ad9ba3e
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-10-14 13:05:34 -0300

    operações aritiméticas com strings

---
+++ programacao_basica/aula9.py
@@ -0,0 +1,8 @@
+concatenacao = 'Matheus' + ' ' + 'Kenji' #sinal de + concatena
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10 #(*) faz a repetição de strings
+print(a_dez_vezes)
+
+tres_vezes_kenji = 3 * 'Kenji'
+print(tres_vezes_kenji)

0e98e1993e6c38bf63d786fce2a5bb9787d92bc1
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-10-14 12:52:10 -0300

    exercício variáveis

---
+++ programacao_basica/aula6.py
@@ -0,0 +1,9 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+#  tipos imutáveis e primitivos:
+# str, int, float, bool
+print(int('1') + 1)
+print('a' + 'b')
+print(type(float('1') + 1))
+print(bool(' '))
---
+++ programacao_basica/aula7.py
@@ -0,0 +1,14 @@
+nome = 'Matheus'
+sobrenome = 'Kenji'
+idade = 21
+ano_nascimento = 2002
+maior_de_idade = True
+altura_metros = 1.70
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)

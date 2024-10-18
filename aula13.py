ef6c9009a63c9ea71ded82563f8a3040811be0f3
MATHEUS KENJI NISHIMURA <matheuskenji12345678@gmail.com>
2024-10-18 11:17:30 -0300

    função input

---
+++ aula13.py
@@ -0,0 +1,8 @@
+# nome = input('Digite seu nome: ')
+# print(f'O seu nome é {nome=}')
+
+numero1 = input('Digite um número: ')
+numero2 = input('Digite o segundo número: ')
+resultado_da_soma = int(numero1) + int(numero2)
+
+print(f'A soma dos dois números é {resultado_da_soma}')
